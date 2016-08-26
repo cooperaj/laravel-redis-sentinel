6c1bbec6b9117a3f7178206baf6e0d8b8f913d6b
Adam Cooper <adam.cooper@nottingham.ac.uk>
2016-08-26 15:22:36 +0100

    Added a replacement Redis driver that allows you to configure the database number for Sentinel clients.

---
+++ src/RedisSentinel/Laravel/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace RedisSentinel\Laravel;
+
+use Illuminate\Support\Arr;
+use Predis\Client;
+use Illuminate\Redis\Database as LaravelRedisDatabase;
+
+class Database extends LaravelRedisDatabase
+{
+    /**
+     * Create an array of single connection or sentinel clients
+     *
+     * @param  array  $servers
+     * @param  array  $options
+     * @return array
+     */
+    protected function createSingleClients(array $servers, array $options = [])
+    {
+        $clients = [];
+
+        foreach ($servers as $key => $server) {
+            $options = array_merge($options, Arr::pull($server, 'options'));
+
+            $clients[$key] = new Client($server, $options);
+        }
+
+        return $clients;
+    }
+}
--- src/RedisSentinel/Laravel/RedisSentinelServiceProvider.php
+++ src/RedisSentinel/Laravel/RedisSentinelServiceProvider.php
@@ -8,6 +8,13 @@ use Illuminate\Support\ServiceProvider;
 class RedisSentinelServiceProvider extends ServiceProvider
 {
     /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
      * Add the connector to the queue drivers
      */
     public function boot()
@@ -20,12 +27,27 @@ class RedisSentinelServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register() {}
+    public function register()
+    {
+        $this->app->singleton('redis', function ($app) {
+            return new Database($app['config']['database.redis']);
+        });
+    }
 
     protected function registerRedisSentinelConnector(QueueManager $manager)
     {
         $manager->addConnector('sentinel-redis', function () {
             return new SentinelConnector($this->app['redis']);
         });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['redis'];
     }
 }
